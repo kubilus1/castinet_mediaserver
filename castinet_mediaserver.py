69d53133f5c7aeabe488e9ddccd20541cf325cd5
Matt Kubilus <mattkubilus@gmail.com>
2014-03-15 21:55:05 -0400

    Added CSS, now using simplegrid.css: http://thisisdallas.github.io/Simple-Grid/ Fixed issues, etc.

--- castinet_mediaserver.py
+++ castinet_mediaserver.py
@@ -21,10 +21,10 @@ import CGIHTTPServer
 import SocketServer
 
 PORT = 8080
-SERVE_DIR='/home/mkubilus/Software/pyblog/myblog'
 
 convert_list = [".avi", ".flv", ".mkv", ".mpg"]
 video_list = [".mp4",]
+allmedia_list = convert_list + video_list
 css=None
 
 class MyTCPServer(SocketServer.ForkingTCPServer):
@@ -34,11 +34,12 @@ class MyTCPServer(SocketServer.ForkingTCPServer):
 
 class MyRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
     def do_GET(self):
-        print self.path
+        print "Looking for:", self.path
         root, ext = os.path.splitext(self.path)
-        print "EXT:", ext
         # Get the local file location
         localpath = urllib.unquote(self.path).lstrip('/')
+
+        DEVNULL = open(os.devnull, 'w')
 
         if ext in convert_list:
             try: 
@@ -49,45 +50,37 @@ class MyRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
                 
                 # Transcode on the fly
                 p0 = subprocess.Popen(
-                    #['ffmpeg', '-i', localpath, '-vcodec', 'libvpx',
-                    #    '-cpu-used', '5', '-deadline', 'realtime', '-s',
-                    #    '640x360', '-f', 'webm', 'pipe:1'
-                    #], stdout=subprocess.PIPE
                     ['ffmpeg', '-i', localpath, '-vcodec', 'libvpx',
                         '-vb', '1M', '-acodec', 'libvorbis',
                         '-deadline', 'realtime', '-s',
                         '640x360', '-f', 'webm', 'pipe:1'
-                    ], stdout=subprocess.PIPE
+                    ], stdout=subprocess.PIPE, stderr=DEVNULL
                 )
-                
+               
+                # Give transcoding a moment
                 time.sleep(1)
 
                 # Read the piped data
                 data = p0.stdout.read(1024)
                 while p0.poll() is None:
                     self.wfile.write(data)
-                    #print "wrote 1024 bytes"
                     data = p0.stdout.read(1024)
-
             finally:
                 p0.terminate()
-        elif (ext == '.png') and (not
+        
+        elif (ext == '.vpng') and (not
                 os.path.isfile(localpath)):
-            print "LOCALPATH:", localpath 
             localdir = os.path.dirname(localpath)
-            #print "GLOBS:", glob.glob("%s.*" % os.path.splitext(localpath)[0])
-            vidfile=glob.glob("%s.*" %
-                    os.path.splitext(localpath)[0].replace('[', '[[]'))[0]
-            print "VIDFILE:", vidfile
-            #vidpath = os.path.join(localdir, vidfile)
+            globstr = "%s*[.avi|.mp4|.flv|.mpg|.mkv]" % os.path.splitext(localpath)[0].replace('[', '?').replace(']', '?')
+            vidfile=glob.glob(globstr)[0]
             vidpath = vidfile
-            print "VIDPATH:", vidpath
+            
             # Snag a picture
-            p0 = subprocess.Popen(
-                    ['ffmpeg', '-ss', '00:03:00', '-i',
-                        vidpath,
+            p0 = subprocess.Popen(['ffmpeg', '-i',
+                        vidpath, '-ss', '15', 
                         '-vframes', '1', '-f', 'image2', 'pipe:1'],
-                    stdout=subprocess.PIPE)
+                    stdout=subprocess.PIPE, stderr=DEVNULL)
+            p0.wait()
             data = p0.stdout.read()
             self.wfile.write(data)
 
@@ -105,19 +98,17 @@ class MyRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
         dirlist=[]
         filelist=[]
 
-        #try:
-        if True:
+        try:
             print "PATH:", path
             for item in os.listdir(path):
-                print "CHECK PATH: ", item
-                if os.path.isfile(os.path.join(path,item)):
+                if os.path.isfile(os.path.join(path,item)) and \
+                        os.path.splitext(item)[1] in allmedia_list:
                     filelist.append(item)
                 elif os.path.isdir(os.path.join(path,item)):
                     dirlist.append(item)
-        #except os.error:
-        #    self.send_error(404, "No permission to list directory")
-        #    print "some kind of error"
-        #    return None
+        except os.error:
+            self.send_error(404, "No permission to list directory")
+            return None
 
         filelist.sort(key=lambda a: a.lower())
         dirlist.sort(key=lambda a: a.lower())
@@ -127,25 +118,23 @@ class MyRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
         f = StringIO()
         f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
         f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
-        #f.write("<head><style> td { padding-left: 2em; } \n")
         f.write('<head><style>')
         f.write(css)
-        f.write('</style></head>')
-        #f.write("th { padding-left: 2em; } </style></head> \n")
+        f.write('</style>')
+        f.write('<meta name="viewport" content="width=device-width" />')    
+        f.write('</head>')
         f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
-        f.write("<hr>\n<table>\n")
-        f.write("<tr><th></th><th></th><th ALIGN=left>Name</th><th>Size</th><th ALIGN=right>Date Modified</th></tr>\n")
+        
 
         full_list = dirlist + filelist
-        print "FULL LIST: ", full_list
-
         for name in full_list:
-            f.write('<tr>\n')
+            f.write("<div class='grid grid-pad'>")
             fullname = os.path.join(path, name)
             stats = os.stat(fullname)
             size = stats[stat.ST_SIZE]
             mtime = time.ctime(stats[stat.ST_MTIME])
             displayname = linkname = name
+            
             # Append / for directories or @ for symbolic links
             if os.path.isdir(fullname):
                 displayname = name + "/"
@@ -153,25 +142,28 @@ class MyRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
             if os.path.islink(fullname):
                 displayname = name + "@"
                 # Note: a link to a directory displays with @ and links with /
-            f.write('<td></td><td>')
+            
+            f.write("<div class='alink clear'>")
+            f.write("<a href='%s'>\n"
+                    % (urllib.quote(linkname))) 
+            f.write("<div class='col-1-4'>")
+            f.write("<div class='content'>")
+            
+            # Request a thumbnail ....
             root,ext = os.path.splitext(urllib.quote(linkname))
             if ext in convert_list or ext in video_list:
-                f.write('<img src="%s" width=100 height=50>' %
-                        "%s.png" % root
+                f.write('<img src="%s" width=150 height=75>\n' %
+                        "%s.vpng" % root
                 )
 
-                #bname=os.path.basename(urllib.quote(linkname))
-                #print "BNAME:", bname
-                #imgurl=self.google_image(bname)
-                #print "URL:", imgurl
-                #f.write('<img src="%s" width=100 height=50>' %
-                #        imgurl
-                #)
+            f.write("</div></div><div class='col-1-3'>")
+            f.write("<div class='content'>")
+            f.write(cgi.escape(displayname))
+            f.write("</div></div></a>")
+            f.write("</div>")    
+            f.write("</div>\n")
                 
-            f.write('</td><td><a href="%s">%s</a></td>\n'
-                    % (urllib.quote(linkname), cgi.escape(displayname)))
-            f.write('<td>%s</td><td>%s</td>' % (size, mtime))
-        f.write("</table>\n<hr>\n</body>\n</html>\n")
+        f.write("</body>\n</html>\n")
         length = f.tell()
         f.seek(0)
 
@@ -181,20 +173,13 @@ class MyRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
         self.end_headers()
         return f
 
-
-    def google_image(self, search):
-        #search = x.split()
-        #search = '%20'.join(map(str, search))
-        url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&safe=off' % search
-        search_results = urllib.urlopen(url)
-        js = json.loads(search_results.read().decode('utf-8'))
-        results = js['responseData']['results']
-        for i in results: rest = i['unescapedUrl']
-        return rest
-
 if __name__ == "__main__":
+
+    # This could be run on any directory so pull in the css now.
     with open('css/main.css') as h:
         css = h.read()
+    with open('css/simplegrid.css') as h:
+        css = css + h.read()
 
     argc = len(sys.argv)
     if argc >= 2:
@@ -202,18 +187,16 @@ if __name__ == "__main__":
         os.chdir(path)
     if argc >= 3:
         PORT = int(sys.argv[2])
+    
+    print "---------------------"
+    print "Castinet Media Server"
+    print "  Now serving %s" % os.getcwd()
 
-    print "Running from %s" % os.getcwd()
-
-    name = ""
+    hostname = ""
     handler = MyRequestHandler
-    httpd = MyTCPServer((name, PORT), handler)
-    httpd.server_name = name
+    httpd = MyTCPServer((hostname, PORT), handler)
+    httpd.server_name = hostname
     httpd.server_port = PORT
 
-    os.putenv("HTTP_HOST","%s:%s" % (name, PORT))
-    os.environ["HTTP_HOST"] = "%s:%s" %(name, PORT)
-    print os.getenv("HTTP_HOST")
-
-    print "Serving at port %s" % PORT
+    print "  at port %s" % PORT
     httpd.serve_forever()
